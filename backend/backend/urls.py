f0f17630f11f0f345dad12c130b7aa6c69ae1a7f
andmerk93 <andmerk93@yandex.ru>
2023-04-07 05:03:24 +0700

    flake8

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-#    
 ]
-
 
 if settings.DEBUG:
     from .schema import schema
