05461c803d1f90046d7c79572b02227cbe9eb0eb
andmerk93 <andmerk93@yandex.ru>
2023-04-07 03:47:43 +0700

    nginx merge left

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,17 @@
 import os
 from pathlib import Path
 
-#from dotenv import load_dotenv
-
-#load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY'),
+DEBUG = False
 
-# DEBUG = True
+RUN_LOCAL = False
+
+if RUN_LOCAL:
+    from dotenv import load_dotenv
+    load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY'),
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS'),
 
@@ -105,6 +107,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -118,7 +124,13 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
+
+if DEBUG:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] += ('rest_framework.renderers.BrowsableAPIRenderer',)
 
 DJOSER = {
     'HIDE_USERS': False,
