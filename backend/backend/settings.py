03561f04bd2bc523d45d6a4fa1cb4c58e68d2c7b
andmerk93 <andmerk93@yandex.ru>
2023-04-07 13:12:52 +0700

    fixed workflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,8 @@ if os.getenv('DB_ENGINE'):
         'default': {
             'ENGINE': os.getenv('DB_ENGINE'),
             'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('POSTGRES_USER'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
             'HOST': os.getenv('DB_HOST'),
             'PORT': os.getenv('DB_PORT'),
         }
