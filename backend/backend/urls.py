c10788257f435ae4553738c17a1d3becd094cabe
andmerk93 <andmerk93@yandex.ru>
2023-04-08 20:26:30 +0700

    working with media

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     from .schema import schema
